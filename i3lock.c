eab31aa3ada8e2b87b803620974514b487b6e1d1
Karl Tarbe <karulont@gmail.com>
2014-03-18 22:13:44 +0200

    Documentation updates

--- i3lock.c
+++ i3lock.c
@@ -760,7 +760,7 @@ int main(int argc, char *argv[]) {
             break;
         default:
             errx(1, "Syntax: i3lock [-v] [-n] [-b] [-d] [-c color] [-u] [-p win|default]"
-            " [-i image.png] [-t] [-e]"
+            " [-i image.png] [-t] [-f] [-e]"
             );
         }
     }
