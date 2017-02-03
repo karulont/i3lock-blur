a00f5827f040763361cb9c54c2de889eb804bccb
Karl Tarbe <karulont@gmail.com>
2017-02-03 03:58:37 +0200

    Remove old declaration

--- blur.h
+++ blur.h
@@ -1,7 +1,7 @@
 #include <X11/Xlib.h>
 #include <cairo.h>
 #include <xcb/xcb.h>
-void blur_image_surface(cairo_surface_t *surface, int radius);
+
 void blur_image_gl(int scr, Pixmap pixmap, int width, int height, int radius,
                    float sigma);
 void glx_init(int scr, int w, int h, int radius, float sigma);
